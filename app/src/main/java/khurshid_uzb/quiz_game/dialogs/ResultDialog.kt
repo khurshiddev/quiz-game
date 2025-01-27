bc7f2a05b37197c3e8a77b0f5f5032bd9a542e1f
khurshiddev <umaribnxattob8008@gmail.com>
2025-01-27 11:03:25 +0900

    feat: add InfoActivity.kt

---
+++ app/src/main/java/khurshid_uzb/quiz_game/dialogs/ResultDialog.kt
@@ -0,0 +1,21 @@
+package khurshid_uzb.quiz_game.dialogs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import khurshid_uzb.quiz_game.R
+
+class ResultDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result_dialog)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
